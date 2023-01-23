d80a81ff3b6cfb08952d660d51d91f1e3add1c52
Brunoo652 <breboredoa@fpcoruna.afundacion.org>
2023-01-23 13:54:33 +0100

    añadido codigo de endpoint.Da error

--- app/src/main/java/com/example/personal_trainer/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/personal_trainer/ui/login/LoginActivity.java
@@ -5,6 +5,9 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -22,15 +25,29 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.personal_trainer.R;
 import com.example.personal_trainer.ui.login.LoginViewModel;
 import com.example.personal_trainer.ui.login.LoginViewModelFactory;
 import com.example.personal_trainer.databinding.ActivityLoginBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
     private ActivityLoginBinding binding;
+    private RequestQueue queue;
+    private EditText username;
+    private EditText password;
+    private Context context= this;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,6 +55,9 @@ public class LoginActivity extends AppCompatActivity {
 
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        username = findViewById(R.id.username);
+        password = findViewById( R.id.password);
 
         loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
@@ -122,6 +142,66 @@ public class LoginActivity extends AppCompatActivity {
                         passwordEditText.getText().toString());
             }
         });
+
+        //lanzar petición post para el inicio de sesión
+
+        queue = Volley.newRequestQueue(this);
+        private void loginUser() {
+            JSONObject requestBody = new JSONObject();
+            try {
+                requestBody.put("username", username.getText().toString() );
+                requestBody.put("password", password.getText().toString() );
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+            JsonObjectRequest request = new JsonObjectRequest(
+                    Request.Method.POST,
+                    "https://mockapi.io/projects/63bd4219fa38d30d85de40e5"+ "/sessions",
+                    requestBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            String receivedToken;
+                            try {
+                                receivedToken = response.getString("sessionToken");
+                            } catch (JSONException e) {
+                                // Si el JSON de la respuesta NO contiene "sessionToken", vamos a lanzar
+                                // una RuntimeException para que la aplicación rompa.
+                                // En preferible que sea NOTORIO el problema del servidor, pues desde
+                                // la aplicación no podemos hacer nada. Estamos 'vendidos'.
+                                throw new RuntimeException(e);
+                            }
+                            // Si la respuesta está OK, mostramos un Toast
+                            // Esta línea asume que private Context context = this; está definido
+                            Toast.makeText(context, "Token: " + receivedToken, Toast.LENGTH_LONG).show();
+                            Intent I = new Intent(context,StatusActivity.class);
+                            context.startActivity(I);
+                            // Instanciamos un objeto de tipo SharedPreferences
+                            // En el constructor pasamos un String. SIEMPRE será el mismo para nuestra aplicación.
+                            SharedPreferences preferences = context.getSharedPreferences("SESSIONS_APP_PREFS", MODE_PRIVATE);
+                            SharedPreferences.Editor editor = preferences.edit();
+                            editor.putString("VALID_USERNAME", username.getText().toString());
+                            editor.putString("VALID_TOKEN", receivedToken);
+                            editor.commit();
+                            finish();
+
+                        }
+
+                    },
+
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            int serverCode = error.networkResponse.statusCode;
+                            Toast.makeText(context,"Código de respuesta: "+ serverCode, Toast.LENGTH_LONG).show();
+                        }
+                    }
+            );this.queue.add(request);
+
+        }
+
+
+
     }
 
     private void updateUiWithUser(LoggedInUserView model) {
@@ -133,4 +213,7 @@ public class LoginActivity extends AppCompatActivity {
     private void showLoginFailed(@StringRes Integer errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
     }
+
+
+
 }
