a811f0b731a80a957ac932e28d07974d3f0fa841
Pablo Dopico <pdopicob@fpcoruna.afundacion.org>
2023-02-01 09:35:41 +0100

    Completada actividad de posteo de ejercicios, faltaría añadir alguna comrobación en los parámetros

--- app/src/main/java/com/example/personal_trainer/addexercise.java
+++ app/src/main/java/com/example/personal_trainer/addexercise.java
@@ -2,6 +2,8 @@ package com.example.personal_trainer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -10,11 +12,22 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class addexercise extends AppCompatActivity {
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class addexercise extends AppCompatActivity {
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        int userId=0;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addexercise);
@@ -25,11 +38,19 @@ public class addexercise extends AppCompatActivity {
         EditText fecha = findViewById(R.id.fecha);
         EditText duracion = findViewById(R.id.duracion);
         Button enviar = findViewById(R.id.enviar);
+        RequestQueue requestQueue = Volley.newRequestQueue(this.getApplicationContext());
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.exercises, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("userInfo", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("userId", userId);
+        editor.commit();
+        userId = sharedPreferences.getInt("userId", 0);
+        String url = "https://63c57b6af3a73b3478575467.mockapi.io/user/" + userId + "/exercises";
 
         enviar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,9 +61,34 @@ public class addexercise extends AppCompatActivity {
                 String duracionR = duracion.getText().toString();
                 String tipoDeEjercicioR = spinner.getSelectedItem().toString();
 
-                Toast.makeText(addexercise.this, "EJERCICIO AÑADIDO: "+"\n" + nombreEjercicioR + "\nFecha: " + fechaR + "\nDuración: "
-                        + duracionR + "\nCalorias quemadas: " + caloriasQuemadasR + "\nTipo: " + tipoDeEjercicioR,
-                        Toast.LENGTH_LONG).show();
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("name", nombreEjercicioR);
+                    jsonObject.put("category", tipoDeEjercicioR);
+                    jsonObject.put("start-date", fechaR);
+                    jsonObject.put("duration", duracionR);
+                    jsonObject.put("calories", caloriasQuemadasR);
+
+                } catch (JSONException e) {
+                }
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.POST,
+                        url,
+                        jsonObject,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(addexercise.this, "Ejercicio añadido", Toast.LENGTH_LONG).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(addexercise.this, "Error al añadir ejercicio", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                requestQueue.add(jsonObjectRequest);
             }
         });
     }
