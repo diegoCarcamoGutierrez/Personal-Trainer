f97153ff266d46091005e71bb560bcd3363c1b8d
Pablo Dopico <pdopicob@fpcoruna.afundacion.org>
2023-02-06 12:32:36 +0100

    Completado fragment AddExercise (en teoria ahora ya deja pushear)

--- app/src/main/java/com/example/personal_trainer/addExercise/AddExerciseFragment.java
+++ app/src/main/java/com/example/personal_trainer/addExercise/AddExerciseFragment.java
@@ -1,66 +1,96 @@
 package com.example.personal_trainer.addExercise;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.personal_trainer.R;
+import androidx.fragment.app.Fragment;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddExerciseFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AddExerciseFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AddExerciseFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddExerciseFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddExerciseFragment newInstance(String param1, String param2) {
-        AddExerciseFragment fragment = new AddExerciseFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    Context context;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Context context = getContext();
+        View view = inflater.inflate(R.layout.fragment_add_exercise,container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_exercise, container, false);
+        Spinner spinner = view.findViewById(R.id.exercise_spinner);
+        EditText ejercicio = view.findViewById(R.id.ejercicio);
+        EditText calorias = view.findViewById(R.id.calorias);
+        EditText fecha = view.findViewById(R.id.fecha);
+        EditText duracion = view.findViewById(R.id.duracion);
+        Button enviar = view.findViewById(R.id.enviar);
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());;
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.exercises, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        int userId = sharedPreferences.getInt("userId", 0);
+        String url = "https://63c57b6af3a73b3478575467.mockapi.io/user/" + userId + "/exercises";
+
+
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombreEjercicioR = ejercicio.getText().toString();
+                String caloriasQuemadasR = calorias.getText().toString();
+                String fechaR = fecha.getText().toString();
+                String duracionR = duracion.getText().toString();
+                String tipoDeEjercicioR = spinner.getSelectedItem().toString();
+
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("name", nombreEjercicioR);
+                    jsonObject.put("category", tipoDeEjercicioR);
+                    jsonObject.put("start-date", fechaR);
+                    jsonObject.put("duration", duracionR);
+                    jsonObject.put("calories", caloriasQuemadasR);
+
+                } catch (JSONException e) {
+                }
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.POST,
+                        url,
+                        jsonObject,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(context.getApplicationContext(), "Ejercicio añadido", Toast.LENGTH_LONG).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context.getApplicationContext(), "Error al añadir ejercicio", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                requestQueue.add(jsonObjectRequest);
+            }
+        });
+        return view;
     }
 }
