635fec15daa26b264a1304a340e132066c7d5176
Pablo Dopico <pdopicob@fpcoruna.afundacion.org>
2023-02-07 10:17:52 +0100

    Avanzado con el recyclerview

--- app/src/main/java/com/example/personal_trainer/history/HistoryFragment.java
+++ app/src/main/java/com/example/personal_trainer/history/HistoryFragment.java
@@ -1,66 +1,112 @@
 package com.example.personal_trainer.history;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.personal_trainer.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HistoryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HistoryFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView recyclerExercises;
+    Exercise_Adapter exercise_adapter;
+    List<Exercise> exerciseList;
 
     public HistoryFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ExeerciseHistory.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HistoryFragment newInstance(String param1, String param2) {
-        HistoryFragment fragment = new HistoryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise_history, container, false);
+
+
+        View view = inflater.inflate(R.layout.fragment_exercise_history, container, false);
+        initialize(view);
+        loadExercises();
+        return view;
     }
+
+    private void initialize(View view) {
+        recyclerExercises = view.findViewById(R.id.recyclerview);
+        recyclerExercises.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        exerciseList = new ArrayList<>();
+        exercise_adapter = new Exercise_Adapter(exerciseList, getActivity());
+        recyclerExercises.setAdapter(exercise_adapter);
+    }
+
+    private void loadExercises() {
+        Context context = getContext();
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences("SESSIONS_APP_PREFS", Context.MODE_PRIVATE);
+
+        int userId = sharedPreferences.getInt("userId",0);
+        String url = "https://63c57b6af3a73b3478575467.mockapi.io/user/" + "1" + "/exercises";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Toast.makeText(context, "Manten pulsado para editar un ejercicio", Toast.LENGTH_LONG).show();
+
+                        List<Exercise> exerciseList = new ArrayList<>();
+
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject exerciseJson = response.getJSONObject(i);
+                                int id = exerciseJson.getInt("id");
+                                String name = exerciseJson.getString("name");
+                                String duration = exerciseJson.getString("duration");
+                                String calories = exerciseJson.getString("calories");
+
+                                Exercise exercise = new Exercise(id, name, duration, calories);
+                                exerciseList.add(exercise);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        exercise_adapter.setExerciseList(exerciseList);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Context context = getContext();
+
+                        Toast.makeText(context, "Se ha producido un error", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+        requestQueue.add(request);
+    }
+
 }
