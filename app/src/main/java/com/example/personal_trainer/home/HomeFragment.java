4ab029cfaee96a4e389fa8dc18cdd697a947879b
Diego Alonso Cárcamo Gutiérrez <dcarcamog@fpcoruna.afundacion.org>
2023-02-07 10:17:29 +0100

    Comienzo del fragmento de inicio.

--- app/src/main/java/com/example/personal_trainer/home/HomeFragment.java
+++ app/src/main/java/com/example/personal_trainer/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.personal_trainer.home;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +9,42 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.personal_trainer.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONArray;
+
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        Context context=getContext();
+        SharedPreferences sharedPreferences = context.getSharedPreferences("SESSIONS_APP_PREFS", Context.MODE_PRIVATE);
+        int userId = sharedPreferences.getInt("userId",0);
+        String url = "https://63c57b6af3a73b3478575467.mockapi.io/user/" + userId + "/exercises";
+        View view=inflater.inflate(R.layout.fragment_home,container,false);
+
+        TextView totalTime=view.findViewById(R.id.totalTime);
+        TextView burnedCalories=view.findViewById(R.id.burnedCalories);
+        TextView completedExercises=view.findViewById(R.id.completedExercises);
+        timeCounter(url);
+        return view;
+
+    }
+
+    private int timeCounter(String url){
+        JsonArrayRequest jsonArrayRequest=new JsonArrayRequest(
+                Request.Method.GET,
+                new Response.Listener<JSONArray>(){
+                    
+                }
+
+        )
     }
 }
